966f2a17c602335dfd2e7dfdc283961c050dc697
hareesh K <hk3886092@gmail.com>
2025-10-16 12:15:20 +0000

    commit 4

--- models/test_model.sql
+++ models/test_model.sql
@@ -5,8 +5,11 @@
 {# -- select {{ modules.datetime.date.today().strftime('%Y-%m-%d') }}  #}
 {# {{ get_current_date() }}    #}
 {# {{ this.name }}  #}
-{# {% set tday = "2025-10-16" %}  #}
-{% set val1 = var('tday') %} 
-{{ log("tday value is: " ~ tday ~'----'~ val1, info=True) }} 
-select '{{tday}}' || '----'|| '{{val1}}' as date_val 
--- SELECT 1
+
+{% set tday = 2025 %}  
+{% set val1 = var('tday',1111) %}
+{% set val2 = tday %}
+{{ log("tday value is: " ~ tday ~"var('tday') value is:"~ val1 ~"tday assigned to val2 value is:"~ val2, info=True) }} 
+select 'tday value is: ' || {{tday}} || 'var(\'tday\') value is:'|| {{val1}} ||'tday assigned to val2 value is:'|| {{val2}} as date_val 
+-- out put is below
+-- tday value is: 2025var('tday') value is:1990tday assigned to val2 value is:2025
