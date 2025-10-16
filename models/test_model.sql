34f93a25a9d9e734b3a9c77ce402096fbe8e0c3c
hareesh K <hk3886092@gmail.com>
2025-10-16 11:55:48 +0000

    commit 3

--- models/test_model.sql
+++ models/test_model.sql
@@ -1,7 +1,12 @@
 /* creating my first model to test QUERY_TAG()  */
---SELECT * FROM {{ source('raw_data1','src_audit') }}
-
---select {{ this.name }} AS PATH
+-- SELECT * FROM {{ source('raw_data1','src_audit') }}
+-- select {{ this.name }} AS PATH
 {# {% val1 = var('tday', get_current_date() %} #}
-
-select {{ get_current_date() }}
+{# -- select {{ modules.datetime.date.today().strftime('%Y-%m-%d') }}  #}
+{# {{ get_current_date() }}    #}
+{# {{ this.name }}  #}
+{# {% set tday = "2025-10-16" %}  #}
+{% set val1 = var('tday') %} 
+{{ log("tday value is: " ~ tday ~'----'~ val1, info=True) }} 
+select '{{tday}}' || '----'|| '{{val1}}' as date_val 
+-- SELECT 1
